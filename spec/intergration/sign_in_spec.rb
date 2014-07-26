6a81eaf9f9e6e597beb9707ea4d8854c3d170d79
hogihung <hogihung@gmail.com>
2014-07-26 10:37:04 -0400

    Minor refactor of specs for sign in and that todos are shown for current user.

--- spec/intergration/sign_in_spec.rb
+++ spec/intergration/sign_in_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 feature 'Sign in as a user' do 
   scenario 'with an email address' do
-    visit root_path
-    fill_in 'Email address', with: 'jane_doe@email.com'
-    click_button 'Sign In'
+    sign_in_as('jane_doe@email.com')
     expect(page).to have_css '.welcome', text: 'Welcome, jane_doe@email.com'
   end
 end
